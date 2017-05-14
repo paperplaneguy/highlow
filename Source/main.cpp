df8dc29d5ca62ecbbd26fc8d7e9c6d427ca6697c
Veer Arjun Singh <arjun@Veers-iMac.local>
2017-05-14 16:08:46 +0530

    Done some error handling in setArgs

--- Source/main.cpp
+++ Source/main.cpp
@@ -12,15 +12,18 @@ using std::vector;
 using options::llimit;
 using options::ulimit;
 
-void setArguments(vector<string> args);
+//The following function sets arguments, and returns false if fails
+bool setArguments(vector<string> args);
 
 int main(int argc, char **argv) {
-  if(argc > 1) (setArguments(vector<string>(argv + 1, argv + argc)));
+  if(argc > 1)
+    if(!setArguments(vector<string>(argv + 1, argv + argc)))
+      return 0;
   HighLow::game();
   return 0;
 }
 
-void setArguments(vector<string> arg_v) {
+bool setArguments(vector<string> arg_v) {
   /* if any argument is passed to the game it will check if the user wants  *
    * to change the amount of guesses per number or the upper and lower      *
    * limit of the random number generated. Other arguments are totally      *
@@ -28,13 +31,16 @@ void setArguments(vector<string> arg_v) {
   for (size_t i(0); i < arg_v.size(); i++) {
     if((arg_v[i] == "-g") || (arg_v[i] == "--guesses"))
       options::totalguesses = stoi(arg_v[++i]);
-    else if ((arg_v[i] == "-l") || (arg_v[i] == "--llimit"))
-      llimit = stoi(arg_v[++i]);
-      else if ((arg_v[i] == "-u") || (arg_v[i] == "--ulimi"))
-        ulimit = stoi(arg_v[++i]);
-        else {
-          std::cerr << "Invalid argument " << arg_v[i] << std::endl;
-          return;
-        }
+    else 
+      if ((arg_v[i] == "-l") || (arg_v[i] == "--llimit"))
+        llimit = stoi(arg_v[++i]);
+        else
+          if ((arg_v[i] == "-u") || (arg_v[i] == "--ulimit"))
+            ulimit = stoi(arg_v[++i]);
+          else {
+            std::cerr << "Invalid argument " << arg_v[i] << std::endl;
+            return false;
+          }
   }
+  return true;
 }
